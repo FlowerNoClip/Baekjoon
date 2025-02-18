02afaac46fea1074c120c1f453320d904b452943
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-02-18 20:54:53 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout << "1" << endl;   // 연산 횟수 (항상 1번 수행)
+    cout << "0" << endl;   // 시간 복잡도 O(1)에서 차수는 0
+    return 0;
+
+}
+
+
