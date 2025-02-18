0ad6766699fb43b8133e1053414e83769a70e435
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-02-18 20:54:07 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define MAX_LEN 1000001
+using namespace std;
+
+
+int dp[MAX_LEN];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin >> N;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    cout << dp[N];
+}
+ 
