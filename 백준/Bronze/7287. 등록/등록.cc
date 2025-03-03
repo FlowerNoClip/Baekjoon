b8f1ec6a608cee20e59b320edd11b7b45456317f
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-03 21:22:12 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout << "118" << endl;
+    cout << "thswl9919" << endl;
+    return 0;
+}
