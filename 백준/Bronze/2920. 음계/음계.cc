24873ac6eb4f93af5b91cd52b040b63be59305a4
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-17 21:44:28 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> notes(8);
+    for (int i = 0; i < 8; i++) {
+        cin >> notes[i];
+    }
+
+    bool ascending = true, descending = true;
+
+    for (int i = 0; i < 7; i++) {
+        if (notes[i] < notes[i + 1]) {
+            descending = false;
+        }
+        else if (notes[i] > notes[i + 1]) {
+            ascending = false;
+        }
+    }
+
+    if (ascending) {
+        cout << "ascending" << endl;
+    }
+    else if (descending) {
+        cout << "descending" << endl;
+    }
+    else {
+        cout << "mixed" << endl;
+    }
+
+    return 0;
+}
