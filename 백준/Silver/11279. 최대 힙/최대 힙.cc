9ffabe0e49835ec42ddbe9ec87e5d30f60f35c33
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-26 22:04:14 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    priority_queue<int> pq;
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int temp;
+        cin >> temp;
+
+        if (temp == 0) {
+            if (pq.empty()) {
+                cout << 0 << "\n";
+            }
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(temp);
+        }
+    }
+
+    return 0;
+}
