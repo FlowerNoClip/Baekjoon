bfd542737c4246f0feb41ba51fbcae505b0e35f5
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-04-07 22:11:16 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,55 @@
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <queue> 
+#include <deque> 
+#include <iterator> 
+#include <list> 
+#include <stack> 
+#include <map> 
+#include <unordered_map>
+#include <set> 
+#include <unordered_set>
+#include <math.h> 
+#define ll long long 
+#define INF 1e9+10 
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N; cin >> N;
+    vector<vector<int>> arr(N, vector<int>(N));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                if (arr[j][i] && arr[i][k])
+                {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
