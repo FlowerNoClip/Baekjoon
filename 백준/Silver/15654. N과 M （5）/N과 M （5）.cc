804de189420024f8ba6c2658c868ef2360738061
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-04-14 17:01:56 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,63 @@
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <queue> 
+#include <deque> 
+#include <iterator> 
+#include <list> 
+#include <stack> 
+#include <map> 
+#include <unordered_map>
+#include <set> 
+#include <unordered_set>
+#include <math.h> 
+#define ll long long 
+#define INF 1e9+10 
+using namespace std;
+
+int N, M;
+vector<int> selected;
+vector<int> arr;
+int check[10];
+
+void backtracking(int start) {
+    if (start == M) { // M개의 숫자를 선택했으면 출력
+        for (int i = 0; i < M; i++)
+        {
+            cout << selected[i]<< " ";
+           
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < N; i++) { // 오름차순 유지
+        if (check[i] == 0)
+        {
+            selected[start] = arr[i];
+            check[i] = 1;
+            backtracking(start + 1);
+            check[i] = 0;
+        }
+        
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    sort(arr.begin(), arr.end());
+    selected.resize(M);
+    backtracking(0); // 1부터 시작
+    return 0;
+
+}
