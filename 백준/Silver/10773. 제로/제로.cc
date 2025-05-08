3228c5be7837a5e23ec43a9bdf9e1b08e6685778
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-05-08 20:55:10 +0900

    [Silver IV] Title: 제로, Time: 4 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,39 +1,49 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <stack>
-
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <queue> 
+#include <deque> 
+#include <iterator> 
+#include <list> 
+#include <stack> 
+#include <map> 
+#include <unordered_map>
+#include <set> 
+#include <unordered_set>
+#include <math.h> 
+#define ll long long 
+#define INF 1e9+10 
 using namespace std;
 
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int T; cin >> T;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N; cin >> N;
     stack<int> st;
-    int temp;
-    while (T--)
+    while (N--)
     {
+        int temp; 
         cin >> temp;
 
-        if (temp != 0 )
+        if (temp != 0)
         {
             st.push(temp);
         }
-        else if (!st.empty() && temp == 0)
+        else if (temp == 0)
         {
             st.pop();
         }
     }
-    int answer = 0;
+    
+    int sum = 0;
+
     while (!st.empty())
     {
-        answer += st.top();
+        sum += st.top();
         st.pop();
     }
 
-    cout << answer << endl;
-    
-    
+    cout << sum << endl;
 }
