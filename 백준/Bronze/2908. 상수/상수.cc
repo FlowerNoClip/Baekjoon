36555fdbc05a5d6e6cc8ab83a16153c20e7cc407
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-07-22 10:27:41 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <queue> 
+#include <deque> 
+#include <iterator> 
+#include <list> 
+#include <stack> 
+#include <map> 
+#include <unordered_map>
+#include <set> 
+#include <unordered_set>
+#include <math.h> 
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long 
+#define INF 1e9+10 
+using namespace std;
+
+int main() {
+    FAST_IO;
+    string a, b;
+    cin >> a >> b;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    int result = max(stoi(a), stoi(b));
+    cout << result;
+}
