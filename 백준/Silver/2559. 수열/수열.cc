ede2daee07e5639b837752545e7dcfa270066a66
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-07-30 13:18:53 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,42 @@
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <queue> 
+#include <deque> 
+#include <iterator> 
+#include <list> 
+#include <stack> 
+#include <map> 
+#include <unordered_map>
+#include <set> 
+#include <unordered_set>
+#include <math.h> 
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long 
+#define INF 1e9+10 
+using namespace std;
+
+int main() {
+    FAST_IO
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> num(N);
+    for(int& a : num) cin >> a;
+
+    int tempsum = 0;
+    for(int i = 0; i < K; i++)
+    {
+        tempsum += num[i];
+    }
+    int maxNum = tempsum;
+    for(int i = K; i < N; i++)
+    {
+        tempsum = tempsum - num[i-K] + num[i];
+        maxNum = max(maxNum, tempsum);
+    }
+    
+    cout << maxNum;
+    return 0;
+}
