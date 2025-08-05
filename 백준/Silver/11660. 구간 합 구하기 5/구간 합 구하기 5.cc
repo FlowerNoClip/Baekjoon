b96b24380d50ae0cce4c5790613ac4219097da7d
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-08-05 14:09:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 6264 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -12,34 +12,29 @@
 #include <set> 
 #include <unordered_set>
 #include <math.h> 
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define ll long long 
 #define INF 1e9+10 
+const ll LLINF = 2e9 + 1;
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-	int N, M;
-	cin >> N >> M;
-
-	vector<vector<int>> A(N + 1, vector<int>(N + 1, 0));
-	vector<vector<int>> D(N + 1, vector<int>(N + 1, 0));
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			cin >> A[i][j];
-
-			D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
-		}
-	}
-
-	for (int i = 0; i < M; i++) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-
-		int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-		cout << result << "\n";
-	}
-
+    FAST_IO;
+    int N, M; cin >> N >> M;
+    vector<vector<int>> arr(N+1, vector<int>(N+1));
+    //vctor<vector<int>> prefix(N+1, vector<int>(N+1));
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= N; j++) {
+            cin >> arr[i][j];
+            arr[i][j] = arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1] + arr[i][j];
+        }
+    }
+    while (M--) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << arr[x2][y2]
+             - arr[x1-1][y2]
+             - arr[x2][y1-1]
+             + arr[x1-1][y1-1] << '\n';
+    }
 }
