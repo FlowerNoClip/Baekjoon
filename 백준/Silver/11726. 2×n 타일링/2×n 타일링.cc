ab1cb6b45cbf13befa06a8cabd165805f641686a
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-08-06 09:22:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,24 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#define MAX_LEN 1000001
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <queue> 
+#include <deque> 
+#include <iterator> 
+#include <list> 
+#include <stack> 
+#include <map> 
+#include <unordered_map>
+#include <set> 
+#include <unordered_set>
+#include <math.h> 
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long 
+#define INF 1e9+10 
+const ll LLINF = 2e9 + 1;
 using namespace std;
 
-
-int dp[MAX_LEN];
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int N;
-    cin >> N;
+    FAST_IO;
+    int N; cin >> N;
+    vector<ll> arr(1002);
 
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= N; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    arr[0] = 1;
+    arr[1] = 2;
+    for(int i = 2; i < arr.size(); i++)
+    {
+        arr[i] = (arr[i-1] + arr[i-2]) % 10007;
     }
-    cout << dp[N];
+
+    cout << arr[N-1];
+
 }
- 
