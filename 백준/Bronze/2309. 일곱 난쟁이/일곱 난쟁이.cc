b5b9b79b084e4b196fa72113dda35560257d569e
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-08-19 16:57:54 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -4,37 +4,7 @@ using namespace std;
 #define ll long long 
 int a[9];
 int n = 9, r = 7;
-void solved()
-{
-    int sum = 0;
-    for(int i = 0; i < r; i++)
-    {
-        sum += a[i];
-    }
 
-    if(sum == 100)
-    {
-        sort(a,a+7);
-        for(int i = 0; i < r; i++) cout << a[i] << "\n";
-        exit(0);
-    }
-}
-void makePermutation(int n, int r, int depth)
-{
-    if(depth == r)
-    {
-        solved();
-        return;
-    }
-    
-
-    for(int i = depth; i < n; i++)
-    {
-        swap(a[i], a[depth]);
-        makePermutation(n,r,depth+1);
-        swap(a[i], a[depth]);        
-    }
-}
 int main()
 {
     FAST_IO;
@@ -44,6 +14,23 @@ int main()
         int temp;
         cin >> a[i];
     }
+    sort(a, a+9);
+    do
+    {
+        int sum = 0;
+        for(int i = 0; i < r; i++)
+        {
+            sum += a[i];
+        }
 
-    makePermutation(n, r, 0);
+        if(sum == 100)
+        {
+            break;
+        }
+    } while (next_permutation(a, a+9));
+    
+    for(int i=0; i < r; i++)
+    {
+        cout << a[i] << '\n';
+    }
 }
