3b4d2a33235b42efa80498a493d087c762b9ce92
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-09-19 19:45:31 +0900

    [Gold IV] Title: 연구소, Time: 276 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,38 +1,39 @@
-#include <iostream> 
-#include <vector> 
-#include <algorithm> 
-#include <queue> 
-using namespace std;
-
+#include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define ll long long
+using namespace std;
 
-int N, M; 
-int dx[] = { 0, 0, 1 , -1 };
-int dy[] = { 1, -1, 0, 0 };
-int cnt = 0;
+int N, M;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int answer = 0;
+vector<vector<int>> arr;
 
-void spreadVirus(vector<vector<int>>& tempBoard) {
-    queue<pair<int, int>> q;
-
-    // 모든 바이러스 위치에서 BFS 시작
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (tempBoard[i][j] == 2) {
-                q.push({i, j});
-            }
+void virus(vector<vector<int>>& tempBoard)
+{
+    queue<pair<int,int>> q;
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            if(tempBoard[i][j] == 2)
+                q.push({i,j});
         }
     }
 
-    while (!q.empty()) {
+    while(!q.empty())
+    {
         auto [x, y] = q.front();
         q.pop();
-        for (int dir = 0; dir < 4; dir++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                if (tempBoard[nx][ny] == 0) {
-                    tempBoard[nx][ny] = 2; // 감염
+        for(int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M)
+            {
+                if(tempBoard[nx][ny] == 0)
+                {
+                    tempBoard[nx][ny] = 2;
                     q.push({nx, ny});
                 }
             }
@@ -40,45 +41,57 @@ void spreadVirus(vector<vector<int>>& tempBoard) {
     }
 }
 
-int getSafeArea(vector<vector<int>>& tempBoard) {
-    int safe = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (tempBoard[i][j] == 0) safe++;
+int safeArea(const vector<vector<int>>& board)
+{
+    int cnt = 0;
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            if(board[i][j] == 0)
+                cnt++;
         }
     }
-    return safe;
+    return cnt;
 }
 
-void makeWall(int wallcnt, vector<vector<int>>& board) {
-    if (wallcnt == 3) {
-        vector<vector<int>> tempBoard = board; // 원본 복사
-        spreadVirus(tempBoard); // 바이러스 확산
-        cnt = max(cnt, getSafeArea(tempBoard)); // 안전구역 최대값 갱신
+void makeWall(int wallCount)
+{
+    if(wallCount == 3)
+    {
+        vector<vector<int>> board = arr;
+        virus(board);
+        answer = max(answer, safeArea(board));
         return;
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (board[i][j] == 0) {
-                board[i][j] = 1; // 벽 설치
-                makeWall(wallcnt + 1, board);
-                board[i][j] = 0; // 원상 복구
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            if(arr[i][j] == 0)
+            {
+                arr[i][j] = 1;
+                makeWall(wallCount + 1);
+                arr[i][j] = 0;
             }
         }
     }
 }
 
-int main() {
-    FAST_IO;
+int main()
+{
+    FAST_IO
     cin >> N >> M;
-    vector<vector<int>> board(N, vector<int>(M));
+    arr.assign(N, vector<int>(M, 0));
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> board[i][j];
-        }
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+            cin >> arr[i][j];
     }
 
-    makeWall(0, board);
-    cout << cnt;
+    makeWall(0);
+    cout << answer << "\n";
+    return 0;
 }
