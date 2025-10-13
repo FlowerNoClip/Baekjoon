c7e3a27068f5034290e25b599e7f5ed41da1247b
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-10-13 10:29:29 +0900

    [Gold IV] Title: 오큰수, Time: 204 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 int n, a[1000004], ret[1000004];
 stack<int> s;
 int main()
 {
+    FAST_IO
     cin >> n;
     memset(ret, -1, sizeof(ret));
     for(int i = 0; i < n; i++)
