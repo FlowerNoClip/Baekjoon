b28aa2f204d4ed979ea907d06c70f3d87ec71d9b
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-10-16 12:29:15 +0900

    [Gold IV] Title: 인구 이동, Time: 412 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -11,69 +11,79 @@ int dy[] = {1, -1, 0, 0};
 inline bool isValid(int y, int x) {
     return (0 <= y && y < n && 0 <= x && x < n && !visited[y][x]);
 }
-
-
-bool bfs(int sy, int sx) {
-    queue<pair<int,int>> q;
-    vector<pair<int,int>> united; 
+bool bfs(int sy, int sx)
+{
+    queue<pair<int, int>> q;
+    vector<pair<int, int>> united;
+    
     q.push({sy, sx});
     visited[sy][sx] = 1;
     united.push_back({sy, sx});
     int sum = board[sy][sx];
-
-    while(!q.empty()) {
-        auto [y, x] = q.front(); q.pop();
-        for (int dir = 0; dir < 4; dir++) {
-            int ny = y + dy[dir];
-            int nx = x + dx[dir];
-            if (!isValid(ny, nx)) continue;
-
-            int diff = abs(board[y][x] - board[ny][nx]);
-            if (l <= diff && diff <= r) {
-                visited[ny][nx] = 1;
+    while(!q.empty())
+    {
+        auto[y,x] = q.front(); q.pop();
+        for(int i = 0; i < 4; i++)
+        {
+           int ny = y + dy[i];
+           int nx = x + dx[i];
+           if(!isValid(ny,nx)) continue;
+           
+           int diff = abs(board[y][x] - board[ny][nx]);
+           if(l <= diff && r >= diff)
+           {
                 q.push({ny, nx});
+                visited[ny][nx] = 1;
                 united.push_back({ny, nx});
                 sum += board[ny][nx];
-            }
+           }
         }
     }
-
-    if (united.size() <= 1) return false;
+    if(united.size() <= 1) return false;
 
     int avg = sum / united.size();
-    for (auto [y, x] : united) {
+
+    for(auto [y,x] : united)
+    {
         board[y][x] = avg;
     }
-
     return true;
 }
+
 
 int main() {
     FAST_IO
     cin >> n >> l >> r;
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
+    for(int i = 0; i < n; i ++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             cin >> board[i][j];
+        }
+    }
 
-    int answer = 0;
-
-    while (true) {
+    int ret = 0;
+    while(true)
+    {
         memset(visited, 0, sizeof(visited));
-        bool moved = false;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    if (bfs(i, j)) moved = true;
+        bool flag = false;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(!visited[i][j])
+                {
+                    if(bfs(i, j)) flag = true;
                 }
             }
         }
 
-        if (!moved) break; // 이동이 없으면 종료
-        answer++;
+        if(!flag) break;
+
+        ret++;
     }
 
-    cout << answer << '\n';
-    return 0;
+    cout << ret << '\n';
 }
