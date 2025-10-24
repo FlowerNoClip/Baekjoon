de26eb46cb6310ad62d2fb06bb95ee5c12e36cc8
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-10-24 18:58:26 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 3576 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int n, check[10];
+char a[20];
+vector<string> ret;
+bool good(char x, char y, char op)
+{
+    if(x < y && op =='<') return true;
+    if(y < x && op == '>') return true;
+    return false;
+}
+void go(int idx, string num)
+{
+    if(idx == n + 1)
+    {
+        ret.push_back(num);
+        return;
+    }
+    for(int i = 0; i <= 9; i++)
+    {
+        if(check[i]) continue;
+        if(idx == 0 || good(num[idx-1], i + '0', a[idx-1]))
+        {
+            check[i] = 1;
+            go(idx+1, num + to_string(i));
+            check[i] = 0;
+        }
+    }
+    return;
+}
+int main()
+{
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    go(0, "");
+    sort(ret.begin(), ret.end());
+    
+    cout << ret.back() << endl << ret.front() << endl;
+    return 0;
+}
