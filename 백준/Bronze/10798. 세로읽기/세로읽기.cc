0a5f60a0ce4101cceab0ec7d78dde65d1a27fd6f
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-10-25 11:31:02 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -1,31 +1,10 @@
 #include <bits/stdc++.h>
-#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-
 int main() {
-    FAST_IO;
-    vector<vector<char>> a(5, vector<char>(15, '*'));
-    for(int i = 0; i < 5; i++)
-    {
-        string temp; cin >> temp;
-        for(int j = 0; j < temp.length(); j++)
-        {
-            if(temp[j] != NULL)
-            {
-                a[i][j] = temp[j];
-            }
-        }
-    }
-    for(int j = 0; j < 15; j++)
-    {
-        for(int i = 0; i < 5; i++)
-        {
-            if(a[i][j] != '*')
-            {
-                cout << a[i][j];
-            }
-        }
-    }
-
-
+    ios::sync_with_stdio(0); cin.tie(0);
+    string a[5];
+    for (int i = 0; i < 5; i++) cin >> a[i];
+    for (int j = 0; j < 15; j++)
+        for (int i = 0; i < 5; i++)
+            if (j < a[i].size()) cout << a[i][j];
 }
