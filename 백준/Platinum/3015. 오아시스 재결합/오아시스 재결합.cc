ee0d44c037a3225a517fd9b31ae9f6a4222933f4
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-10-31 20:04:00 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 292 ms, Memory: 10304 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+ll n, ret ,temp;
+stack<pair<ll, ll>> s;
+
+int main()
+{
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+       cin >> temp;
+       int cnt = 1;
+       while(s.size() && s.top().first <= temp)
+       {
+            ret += s.top().second;
+            if(s.top().first == temp)
+            {
+                cnt = s.top().second + 1;
+            }
+            else
+            {
+                cnt = 1;
+            }
+            s.pop();
+       }
+       if(s.size()) ret++;
+       s.push({temp, cnt});
+    }
+
+    cout << ret;
+}
