83555f3b5f440e58701080559c2573ab7256ecab
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-11-01 18:41:11 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * b ;
+}
