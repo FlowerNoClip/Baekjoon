fa40cd3f8541af2ae0828c19b7bf533b3af53db2
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-11-03 15:13:48 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int n, value, day, result;
+
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main()
+{
+    FAST_IO
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> value >> day; v.push_back({day, value});
+
+        // first = day, second = value
+    }
+
+    sort(v.begin(), v.end());
+    for(int i = 0; i < n; i++)
+    {
+        pq.push(v[i].second);
+        {
+            if(pq.size() > v[i].first)
+            {
+                pq.pop();
+            }
+        }
+    }
+    while(pq.size())
+    {
+        result += pq.top(); pq.pop();
+    }
+    cout << result;
+}
