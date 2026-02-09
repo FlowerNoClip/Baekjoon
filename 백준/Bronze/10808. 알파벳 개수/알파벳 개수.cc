836e8c2575a373dd1fd6644b6d57d71fdd5419ea
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-02-09 21:29:40 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,22 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define FAST_IO ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
 using namespace std;
-#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long 
 
-int main()
-{
-    FAST_IO;
-    string str;
+int main() {
+	FAST_IO
+	string str; 
     cin >> str;
-    int a[26] = {0};
 
-    for(int c : str)
-    {
-        a[c-'a']++;
-    }
-    
-    for(int i = 0; i < 26; i++)
-    {
-        cout << a[i] << " ";
-    }
+	int arr[26] = {0};
+
+	for(int i = 0; i < str.size(); i++)
+	{
+		arr[str[i] - 'a']++;
+	}
+
+	for(int i = 0; i < 26; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
 }
