e443fc147395dfab01df2eea7c6dba5e6f701472
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-02-11 22:14:57 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,37 +1,41 @@
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <iostream>
+#include <bits/stdc++.h>
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int N, M;
-    cin >> N;
-
-    while (N--) {
-        unordered_map<string, int> map; // 각 반복마다 초기화
-        int answer = 1; // 초기값 설정
+int fac(int n)
+{
+    if(n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * fac(n-1); 
+}
+int main()
+{
+    FAST_IO
+    int N, M; cin >> N;
+    
+    
+    while(N--)
+    {
+        unordered_map<string, int> cloths;
         cin >> M;
-        for (int i = 0; i < M; i++) {
-            string item, category;
-            cin >> item >> category;
-            map[category]++;
+        int sum = 1;
+        string str;
+        for(int i = 0; i < M; i++)
+        {
+            string item, cate;
+            cin >> item >> cate;
+            cloths[cate]++;
         }
 
-
-        for (auto& pair : map) {
+        for(auto& pair : cloths)
+        {
             string key = pair.first;
             int value = pair.second;
-            answer *= (value + 1);
+            sum *= (value + 1);
         }
-
-
-        cout << answer - 1 << '\n'; // 모든 경우의 수 계산 후, 1을 빼줌 (아무것도 선택하지 않는 경우 제외)
+        cout << sum-1 << '\n';
     }
+    
 
-    return 0;
 }
