a26cec65e222aef69902d7d211b0c113638987ae
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-02-26 22:24:01 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,68 +1,76 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <climits>
+#include <bits/stdc++.h>
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-
-int checkboard(int x, int y, vector<vector<int>>& board) {
-    int startColor = board[x][y]; // 시작 색상
-    int cnt = 0;
-
-    for (int i = x; i < x + 8; i++) {
-        for (int j = y; j < y + 8; j++) {
-            if ((i + j) % 2 == 0 && board[i][j] != startColor) {
-                cnt++;
+int check(int x, int y, vector<vector<int>>& board)
+{
+    int start = board[x][y];
+    int cost = 0;
+    for(int i = x; i < x + 8; i++)
+    {
+        for(int j = y; j < y+8; j++)
+        {
+            if((i+j) % 2 == 0 && board[i][j] != start)
+            {
+                cost++;
             }
-            else if ((i + j) % 2 == 1 && board[i][j] == startColor) {
-                cnt++;
-            }
+            else if((i + j) % 2 == 1 && board[i][j] == start)
+            {
+                cost++;
+            }  
         }
     }
-    return cnt;
+    return cost;
 }
 
-int checkboardReverse(int x, int y, vector<vector<int>>& board) {
-    int startColor = 1 - board[x][y]; // 시작 색 반전
-    int cnt = 0;
-
-    for (int i = x; i < x + 8; i++) {
-        for (int j = y; j < y + 8; j++) {
-            if ((i + j) % 2 == 0 && board[i][j] != startColor) {
-                cnt++;
+int reverseCheck(int x, int y, vector<vector<int>>& board)
+{
+    int start = 1-board[x][y];
+    int cost = 0;
+    for(int i = x; i < x + 8; i++)
+    {
+        for(int j = y; j < y+8; j++)
+        {
+            if((i+j) % 2 == 0 && board[i][j] != start)
+            {
+                cost++;
             }
-            else if ((i + j) % 2 == 1 && board[i][j] == startColor) {
-                cnt++;
-            }
+            else if((i + j) % 2 == 1 && board[i][j] == start)
+            {
+                cost++;
+            }  
         }
     }
-    return cnt;
+    return cost;
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int x, y;
-    cin >> x >> y;
+int main()
+{
+    FAST_IO
+    int N, M;
     char temp;
-    vector<vector<int>> board(x, vector<int>(y));
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
+    int result = 1e9;
+    cin >> N >> M;
+    vector<vector<int>> board(N,vector<int>(M));
+    for(int i = 0; i < N; i++)
+    {   
+        for(int j = 0; j < M; j++)
+        {
             cin >> temp;
-            board[i][j] = (temp == 'W') ? 0 : 1; // W=0, B=1
+            board[i][j] = (temp == 'W') ? 0 : 1;
         }
     }
-
-    int minCost = INT_MAX;
-    for (int i = 0; i <= x - 8; i++) {
-        for (int j = 0; j <= y - 8; j++) {
-            int cost = min(checkboard(i, j, board), checkboardReverse(i, j, board));
-            if (minCost > cost) {
-                minCost = cost;
-            }
+    
+    for(int i = 0; i <= N-8; i++)
+    {
+        for(int j = 0; j <= M-8; j++)
+        {
+            int cost = cost = min(check(i,j,board), reverseCheck(i,j,board));
+            
+            result = min(result, cost);
         }
     }
+    cout << result;
 
-    cout << minCost << "\n";
-    return 0;
+
+    
 }
