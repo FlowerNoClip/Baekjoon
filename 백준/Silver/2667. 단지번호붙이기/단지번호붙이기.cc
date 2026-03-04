25edf39481ff084c3cd61201608ac57e6a9714b4
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-04 21:52:30 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,63 +1,63 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-int cnt = 0;
-vector<int> v;
-bool visited[101][101] = {false};
-int N;
-
-void dfs(int x, int y, vector<vector<int>>& adj) {
-    visited[x][y] = true;
+const int dx[4] = {0, 0, 1, -1};
+const int dy[4] = {1, -1, 0, 0};
+int dfs(int y, int x, vector<vector<int>> &board, vector<vector<bool>> &visited,int &cnt)
+{
+    visited[y][x] = true;
     cnt++;
-    for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
+    for(int i = 0; i < 4; i++)
+    {
         int ny = y + dy[i];
-        if (nx >= 0 && nx < N && ny >= 0 && ny < N) {  // 0-based 범위 체크
-            if (adj[nx][ny] && !visited[nx][ny]) {
-                dfs(nx, ny, adj);
+        int nx = x + dx[i];
+        if(ny >= 0 && ny < board.size() && nx >= 0 && nx < board[0].size())
+        {
+            if(!visited[ny][nx] && board[ny][nx] == 1)
+            {
+                dfs(ny,nx,board,visited,cnt);
+                visited[ny][nx] = 1;   
             }
         }
     }
+    return cnt;
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
+int main()
+{
+    FAST_IO
+    int N;
     cin >> N;
-    vector<vector<int>> adj(N, vector<int>(N));  // N x N 배열
-
-    for (int i = 0; i < N; i++) {
-        string row;
-        cin >> row;
-        for (int j = 0; j < N; j++) {
-            adj[i][j] = row[j] - '0';  // 0-based 배열에 값 입력
+    vector<vector<int>> board(N,vector<int>(N,0));
+    vector<vector<bool>> visited(N,vector<bool>(N,false));
+    int allDie = 0;
+    for(int i = 0; i < N; i++)
+    {
+        string temp;
+        cin >> temp;
+        for(int j = 0; j < temp.size(); j++)
+        {
+            int a = temp[j] - '0';
+            board[i][j] = a;
         }
     }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (adj[i][j] && !visited[i][j]) {
-                cnt = 0;
-                dfs(i, j, adj);
-                if (cnt > 0) {  // 크기가 1인 단지도 포함
-                    v.push_back(cnt);
-                }
+    vector<int> result;
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            if(!visited[i][j] && board[i][j] == 1)
+            {
+                allDie++;
+                int cnt = 0;
+                result.push_back(dfs(i,j,board,visited,cnt));
             }
         }
     }
-
-    sort(v.begin(), v.end());
-    cout << v.size() << endl;
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i] << endl;
+    sort(result.begin(), result.end());
+    cout << allDie<< '\n';
+    for(int tempInt : result)
+    {
+        cout << tempInt << '\n';
     }
-
-    return 0;
 }
