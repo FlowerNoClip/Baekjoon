4b892d24ba71e8a547f08124828ced8095267e2b
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-06 22:04:49 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,62 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-
+#include <bits/stdc++.h>
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-int cnt = 0;
-int dx[4] = { 0, 0, 1, -1 };
-int dy[4] = { 1, -1, 0, 0 };
-int N, M;
-int check[101][101]; 
+const int dy[4] = {1, -1, 0, 0};
+const int dx[4] = {0 ,0, 1, -1};
+int main()
+{
+    FAST_IO
+    int N, M; cin >> N >> M;
+    vector<vector<int>> board(N, vector<int>(M));
+    vector<vector<int>> dist(N, vector<int>(M));
 
+    for(int i = 0; i < N; i++)
+    {
+        string temp; cin >> temp;
+        for(int j = 0; j < M; j++)
+        {
+            board[i][j] = temp[j] - '0';
+        }
+    }
 
-void bfs(int x, int y, vector<vector<int>>& adj, vector<vector<bool>>& visited) {
     queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
-    
-    while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
+    q.push({0,0});
+    dist[0][0] = 1;
+    while(!q.empty())
+    {
+        auto [y,x] = q.front();
         q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
+        if(y == N-1 && x == M - 1)
+        {
+            cout << dist[y][x] << '\n';
+            return 0;
+        }
+        for(int i = 0; i < 4; i++)
+        {
             int ny = y + dy[i];
-            if (nx < 0 || nx >= adj.size() || ny < 0 || ny >= adj[0].size()) {
-                continue;
-            }
-            if (adj[nx][ny] && !visited[nx][ny]) {
-                check[nx][ny] = check[x][y] + 1;
-                visited[nx][ny] = true;
-                q.push(make_pair(nx, ny));
+            int nx = x + dx[i];
+            if(ny >= 0 && ny < N && nx >= 0 && nx < M && !dist[ny][nx] && board[ny][nx] == 1)
+            {
+                q.push({ny, nx});
+                dist[ny][nx] = dist[y][x] + 1;
             }
         }
+        
     }
-
-
-
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> N >> M;
-    vector<vector<int>> adj(N, vector<int>(M));
-    vector<vector<bool>> visited(N, vector<bool>(M, false));
-
-    for (int i = 0; i < N; i++) {
-        string str;
-        cin >> str;
-        for (int j = 0; j < M; j++) {
-            adj[i][j] = str[j] - '0';
-        }
-    }
-
-
-    bfs(0, 0, adj, visited);
-    
-    cout << check[N - 1][M - 1] + 1;
-
-    
 }
