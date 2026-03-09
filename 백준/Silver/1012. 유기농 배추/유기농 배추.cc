2c2562c2e2f6000d1e761e5518ade7e75acf79bd
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-09 20:59:36 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,59 +1,54 @@
 #include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long 
 using namespace std;
-const int max_n = 54;
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-int visited[max_n][max_n] = {0};
-void bfs(int x, int y, vector<vector<int>>& board)
-{
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    visited[x][y] = 1;
-    while(q.size())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for(int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            //if(nx > 0 || nx >= board[0].size() || ny < 0 || ny >= board.size() || board[nx][ny] == 0) continue;
-            if(nx < 0 || nx >= board.size() || ny < 0 || ny >= board[0].size() || board[nx][ny] == 0) continue;
-
-            if(visited[nx][ny]) continue;
-
-            q.push({nx, ny});
-            visited[nx][ny] = 1;
-        }
-    }
-
-}
+const int dy[4] = {1, -1, 0, 0};
+const int dx[4] = {0, 0, 1, -1};
 int main()
 {
-    int T; cin >> T;
+    FAST_IO
+    int T, M, N, K;
+    cin >> T;
     while(T--)
     {
-        int cnt = 0;
-        memset(visited, 0, sizeof(visited));
-        int N, M, S; cin >> N >> M >> S;
-        vector<vector<int>> arr(N, vector<int>(M));
-
-        for(int i = 0; i < S; i++)
+        queue<pair<int, int>> q;
+        cin >> M >> N >> K;
+        vector<vector<int>> plow(N, vector<int>(M));
+        vector<vector<bool>> visited(N, vector<bool>(M));
+        int x, y;
+        for(int i = 0; i < K; i++)
         {
-            int a, b; cin >> a >> b;
-            arr[a][b] = 1;
+            cin >> x >> y;
+            plow[y][x] = 1;
         }
-        for(int i = 0; i < arr.size(); i++)
+        int cnt = 0;
+        for(int i = 0; i < N; i++)
         {
-            for(int j = 0; j < arr[0].size(); j++)
+            for(int j = 0; j < M; j++)
             {
-                if(!visited[i][j] && arr[i][j] == 1)
+                if(plow[i][j] == 1 && !visited[i][j])
                 {
-                    bfs(i,j,arr);
                     cnt++;
+                    q.push({i,j});
+                    while(!q.empty())
+                    {
+                        
+                        auto[y,x] = q.front();
+                        visited[y][x] = true;
+                        q.pop();
+                        for(int i = 0; i < 4; i++)
+                        {
+                            int ny = y + dy[i];
+                            int nx = x + dx[i];
+                            if(ny >= 0 && ny < N && nx >= 0 && nx < M)
+                            {
+                                if(!visited[ny][nx] && plow[ny][nx] == 1)
+                                {
+                                    visited[ny][nx] = true;
+                                    q.push({ny,nx});
+                                }
+                            }
+                        }         
+                    }
                 }
             }
         }
