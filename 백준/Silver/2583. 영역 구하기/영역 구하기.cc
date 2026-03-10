4a5002c35088d8536e31790076128c91ef2eb979
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-10 20:59:40 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,84 +1,77 @@
 #include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long
 using namespace std;
-
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-
-bool check(int nx, int ny, vector<vector<int>>& board, vector<vector<bool>>& visited)
-{
-    int M = board.size();
-    int N = board[0].size();
-    if (nx < 0 || nx >= M || ny < 0 || ny >= N) return false; 
-    if (board[nx][ny] == 1) return false; 
-    if (visited[nx][ny]) return false; 
-    return true;
-}
-
-int bfs(int sX, int sY, vector<vector<int>>& board, vector<vector<bool>>& visited)
-{
-    queue<pair<int,int>> q;
-    q.push({sX, sY});
-    visited[sX][sY] = true;
-    int cnt = 1; 
-
-    while(!q.empty())
-    {
-        auto [x, y] = q.front();
-        q.pop();
-        for(int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(!check(nx, ny, board, visited)) continue;
-
-            q.push({nx, ny});
-            visited[nx][ny] = true;
-            cnt++;
-        }
-    }
-
-    return cnt;
-}
-
+const int dy[4] = {0, 0, -1, 1};
+const int dx[4] = {1, -1, 0, 0};
 int main()
 {
     FAST_IO
-    int M, N, K; 
-    cin >> M >> N >> K; 
+    int M, N, K; cin >> M >> N >> K;
     vector<vector<int>> board(M, vector<int>(N, 0));
     vector<vector<bool>> visited(M, vector<bool>(N, false));
 
-    for(int i = 0; i < K; i++)
+
+
+    for(int j = 0; j < K; j++)
     {
-        int x1, y1, x2, y2; 
-        cin >> x1 >> y1 >> x2 >> y2;
-        for(int y = y1; y < y2; y++)
+        int lx, ly, rx, ry;
+        cin >> lx >> ly >> rx >>ry;
+        for(int i = lx; i < rx; i++)
         {
-            for(int x = x1; x < x2; x++)
+            for(int q = ly; q < ry; q++)
             {
-                board[y][x] = 1;
+                board[q][i] = 1;
             }
         }
     }
 
-    vector<int> answer;
-
-
+    int cnt = 0;
+    vector<int> ret;
     for(int i = 0; i < M; i++)
     {
         for(int j = 0; j < N; j++)
         {
             if(board[i][j] == 0 && !visited[i][j])
             {
-                answer.push_back(bfs(i, j, board, visited));
+                int range = 1;
+                cnt++;
+                queue<pair<int, int>> q;
+                q.push({i,j});
+                visited[i][j] = true;
+                while(!q.empty())
+                {
+                    auto[y,x] = q.front();
+                    q.pop();
+                    
+                    for(int i = 0; i < 4; i++)
+                    {
+                        int ny = y + dy[i];
+                        int nx = x + dx[i];
+                        if(ny >= 0 && ny < M && nx >= 0 && nx < N)
+                        {
+                            if(!visited[ny][nx] && board[ny][nx] == 0)
+                            {
+                                range++;
+                                visited[ny][nx] = true;
+                                q.push({ny,nx});
+                                
+                            }
+                        }
+                    }
+                }
+                ret.push_back(range);
             }
         }
     }
+    
+    cout << cnt << '\n';
 
-    sort(answer.begin(), answer.end());
+    sort(ret.begin(), ret.end());
 
-    cout << answer.size() << '\n';
-    for(int a : answer) cout << a << ' ';
+    for(int a : ret)
+    {
+        cout << a << " ";
+    }
+    
+
 }
