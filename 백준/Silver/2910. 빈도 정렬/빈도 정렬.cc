a785faae0d33c86fa665f108fa965213016d8e3e
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-12 21:33:04 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long
-#include <unordered_map>
 using namespace std;
-vector<pair<int,int>> v;
-map<int, int> mp, mp_first;
-int a[1004];
-bool cmp(pair<int,int> a, pair<int, int> b)
-{
-    if(a.first == b.first)
-    {
-        return mp_first[a.second] < mp_first[b.second];
-    }
-
-    return a.first > b.first;
-}
 int main()
 {
     FAST_IO
-    int N, M; cin >> N >> M;
-    
-    for(int i = 0; i < N; i++)
+    int N, M;
+    cin >> N >> M;
+    vector<int> v;
+    map<int, int> cMap;
+    map<int, int> dMap;
+    int idx = 0;
+    for(int i= 0; i < N; i++)
     {
-        cin >> a[i];
-        mp[a[i]] ++;
-        if(mp_first[a[i]] == 0) mp_first[a[i]] = i + 1;
-    }
-
-    for(auto it : mp)
-    {
-        v.push_back({it.second, it.first});
-    }
-    sort(v.begin(), v.end(), cmp);
-    for(auto i : v)
-    {
-        for(int j = 0; j < i.first; j++)
+        int a; cin >> a;
+        v.push_back(a);
+        cMap[a]++;
+        if(dMap.count(a) == 0)
         {
-            cout << i.second << " ";
+            dMap[a] = idx++;
         }
     }
+    vector<pair<int, int>> vec(cMap.begin(), cMap.end());
+    sort(vec.begin(), vec.end(), [&](const auto &a, const auto &b) 
+    {
+       if(a.second != b.second)
+       {
+        return a.second > b.second;
+       }
 
+       return dMap[a.first] < dMap[b.first];
+    });
+    for(auto i : vec)
+    {
+        for(int j = 0; j < i.second; j++)
+        {
+            cout << i.first << " " ;
+        }
+    }
 }
