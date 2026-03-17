2d1c42f11ef1f7919fba9e202a0cbafab4084785
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-17 22:03:30 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,41 +1,44 @@
 #include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long
 using namespace std;
-int N,o,A,B, asum, bsum;
-string s;
-string pre;
-string print(int a)
+int N, team, sT1, sT2, lT1, lT2;
+int prevT;
+
+string toStr(int t)
 {
-    string d = "00" + to_string(a / 60);
-    string e = "00" + to_string(a % 60);
-    return d.substr(d.size()/2, 2) + ":" + e.substr(e.size() -2, 2);
+    int m = t/60, s = t%60;
+    return (m < 10 ? "0" : "") + to_string(m) + ":" + (s < 10 ? "0" : "") + to_string(s);
 }
-int changeToInt(string a)
+
+int toSec(const string &t)
 {
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3,2).c_str());
+    return (t[0] - '0')*600 + (t[1]-'0') * 60 + (t[3] -'0')*10 + (t[4]-'0');
 }
-void go(int &sum , string s)
-{
-    sum += (changeToInt(s) - changeToInt(pre));
-}
+
 int main()
 {
-    FAST_IO
     cin >> N;
     for(int i = 0; i < N; i++)
     {
-        cin >> o >> s;
-        if(A>B) go(asum, s);
-        else if(B > A) go(bsum,s);
-        o == 1 ? A++ : B++;
-        pre = s;
+        string t;
+        cin >> team >> t;
+
+        int curT = toSec(t);
+
+        if(sT1 > sT2) lT1 += (curT - prevT);
+        else if(sT1 < sT2) lT2 += (curT - prevT);
+
+        if(team == 1) sT1++;
+        else sT2++;
+
+        prevT = curT;
     }
 
-    if (A>B) go(asum, "48:00");
-    else if(B > A) go(bsum, "48:00");
-    cout << print(asum) << "\n";
-    cout << print(bsum) << "\n";
+    int endT = 48 * 60;
 
+    if(sT1 > sT2) lT1 += (endT - prevT);
+    else if(sT2 > sT1) lT2 += (endT - prevT);
 
+    cout << toStr(lT1) << '\n';
+    cout << toStr(lT2) << '\n';
 }
