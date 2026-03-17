d21cd60e07ff3d602c739af13255bbb6ae5d3657
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-17 21:00:39 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,54 +1,76 @@
 #include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-
-string vowels = "aeiou";
-
-bool isVowel(char c) {
-    return vowels.find(c) != string::npos;
-}
-
-bool hasVowel(const string& str) {
-    for (char c : str) if (isVowel(c)) return true;
+bool isHaveMo(string temp)
+{
+    for(char c : temp)
+    {
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c=='u')
+        {
+            return true;
+        }
+    }
     return false;
 }
+bool isTriple(string temp)
+{
+   for(int i = 1; i < temp.length(); i++)
+   {
+        if(temp[i] == temp[i-1])
+        {
+            if(temp[i] != 'e' && temp[i] != 'o')
+            {
+                return false;
+            } 
+        }    
+    }
+   return true;   
+}
 
-bool noDoubleExceptEO(const string& str) {
-    for (int i = 1; i < (int)str.size(); i++) {
-        if (str[i] == str[i-1] && str[i] != 'e' && str[i] != 'o') {
-            return false;
+bool isSequence(string temp)
+{
+    int s = 0;
+    int mos = 0;
+    for(char c : temp)
+    {
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c=='u')
+        {
+            s = 0;
+            mos++;
         }
+        else
+        {
+            mos = 0;
+            s++;
+        }
+
+        if(mos >= 3 || s >= 3)
+        return false;
     }
     return true;
 }
-
-bool noThreeConsecutive(const string& str) {
-    int cnt = 1;
-    for (int i = 1; i < (int)str.size(); i++) {
-        if ((isVowel(str[i]) && isVowel(str[i-1])) ||
-            (!isVowel(str[i]) && !isVowel(str[i-1]))) {
-            cnt++;
-        } else {
-            cnt = 1;
-        }
-        if (cnt >= 3) return false;
-    }
-    return true;
-}
-
-void printResult(const string& word, bool ok) {
-    cout << '<' << word << '>' 
-         << (ok ? " is acceptable." : " is not acceptable.") 
-         << '\n';
-}
-
-int main() {
+int main()
+{
+    
     FAST_IO
-    string word;
-    while (cin >> word) {
-        if (word == "end") break;
-
-        bool ok = hasVowel(word) && noDoubleExceptEO(word) && noThreeConsecutive(word);
-        printResult(word, ok);
-    }
+    /*
+        1. 모음(a,e,i,o,u) 하나를 반드시 포함하여야 한다.
+        2. 모음이 3개 혹은 자음이 3개 연속으로 오면 안 된다.
+        3. 같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
+    */
+   //모음이 한개라도 존재 하는지 ? 
+   while(true)
+   {
+        string temp; cin >> temp;
+        if(temp == "end") return 0;
+        if(isHaveMo(temp) && isSequence(temp) && isTriple(temp))
+        {
+            
+            cout << "<" << temp << "> " << "is " << "acceptable." <<'\n';
+        }          
+        else
+        {
+            cout << "<" << temp << "> " << "is " << "not " << "acceptable." <<'\n';
+        }
+   }
 }
