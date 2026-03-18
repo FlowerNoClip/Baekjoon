0fd8697c21153f874a54887ea066b6000b4e8f32
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-18 21:53:13 +0900

    [Gold IV] Title: 연구소, Time: 352 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,97 +1,103 @@
 #include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long
+#define endl '\n'
 using namespace std;
-
-int N, M;
-int dx[] = {0,0,1,-1};
-int dy[] = {1,-1,0,0};
-int answer = 0;
-vector<vector<int>> arr;
-
-void virus(vector<vector<int>>& tempBoard)
+int ret = 0;
+const int dy[4] = {0, 0, 1, -1};
+const int dx[4] = {1, -1, 0, 0};
+vector<vector<int>> board;
+void virus(vector<vector<int>> &tboard)
 {
-    queue<pair<int,int>> q;
-    for(int i = 0; i < N; i++)
+    
+    queue<pair<int, int>> q;
+    for(int i = 0; i < board.size(); i++)
     {
-        for(int j = 0; j < M; j++)
+        for(int j = 0; j < board[0].size(); j++)
         {
-            if(tempBoard[i][j] == 2)
+            if(tboard[i][j] == 2)
+            {
                 q.push({i,j});
+            }
         }
     }
 
     while(!q.empty())
     {
-        auto [x, y] = q.front();
+        auto [y,x] = q.front();
         q.pop();
         for(int i = 0; i < 4; i++)
         {
-            int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx >= 0 && nx < N && ny >= 0 && ny < M)
+            int nx = x + dx[i];
+            if(ny >= 0 && ny < board.size() && nx >= 0 && nx < board[0].size())
             {
-                if(tempBoard[nx][ny] == 0)
+                if(tboard[ny][nx] == 0)
                 {
-                    tempBoard[nx][ny] = 2;
-                    q.push({nx, ny});
+                    tboard[ny][nx] = 2;
+                    q.push({ny, nx});
                 }
             }
         }
     }
 }
-
-int safeArea(const vector<vector<int>>& board)
+int safe(vector<vector<int>> &tboard)
 {
     int cnt = 0;
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < board.size(); i++)
     {
-        for(int j = 0; j < M; j++)
+        for(int j = 0; j < board[0].size(); j++)
         {
-            if(board[i][j] == 0)
+            if(tboard[i][j] == 0)
+            {
                 cnt++;
+            }
         }
     }
     return cnt;
 }
 
-void makeWall(int wallCount)
+void makeWall(int wallNum)
 {
-    if(wallCount == 3)
+    
+    if(wallNum == 3)
     {
-        vector<vector<int>> board = arr;
-        virus(board);
-        answer = max(answer, safeArea(board));
+        vector<vector<int>> cBoard = board;
+        virus(cBoard);
+        ret = max(ret,safe(cBoard));
         return;
     }
 
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < board.size(); i++)
     {
-        for(int j = 0; j < M; j++)
+        for(int j = 0; j < board[0].size(); j++)
         {
-            if(arr[i][j] == 0)
+            if(board[i][j] == 0)
             {
-                arr[i][j] = 1;
-                makeWall(wallCount + 1);
-                arr[i][j] = 0;
+                board[i][j] = 1;
+                makeWall(wallNum + 1);
+                board[i][j] = 0;
             }
         }
     }
 }
-
 int main()
 {
     FAST_IO
-    cin >> N >> M;
-    arr.assign(N, vector<int>(M, 0));
-
+    int N, M; cin >> N >> M;
+    board.assign(N, vector<int>(M,0));
+    
     for(int i = 0; i < N; i++)
     {
         for(int j = 0; j < M; j++)
-            cin >> arr[i][j];
+        {
+            cin >> board[i][j];
+        }
     }
-
     makeWall(0);
-    cout << answer << "\n";
-    return 0;
+    cout << ret << endl;
+    // 1. 벽 3개 무조건 세우기
+    // 2. 벽 세운 뒤 바이러스 퍼지기
+    // 3. 바이러스 퍼지고 난 뒤에 안전영역 숫자 세기
+    // 4. 반복
+
 }
