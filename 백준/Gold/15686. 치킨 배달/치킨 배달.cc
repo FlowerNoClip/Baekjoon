32e1d4c2d85a3d276e09a8024e87fcbe610c7aad
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-23 22:25:18 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,52 +1,77 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-int n, m, a[54][54], result = 987654321;
-vector<vector<int>> chickenList;
-vector<pair<int, int>> _home, chicken;
-void combi(int start, vector<int>& v)
+#define endl '\n'
+using namespace std;
+vector<vector<int>> board;
+vector<vector<int>> chicken;
+vector<pair<int, int>> sV, hV;          
+const int dy[4] = {0, 0, 1, -1};
+const int dx[4] = {1, -1, 0, 0};
+int shopCount = 0;
+int maxNum = -1;
+int N, M;
+int res = 1e9; 
+
+void DeleteShop(int a, vector<int>& v)
 {
-    if(v.size() == m)
+    if(v.size() == M)
     {
-        chickenList.push_back(v);
+        chicken.push_back(v);
         return;
-    }
-    for(int i = start+1; i < chicken.size(); i++)
+    };
+
+    for(int i = a+1; i < sV.size(); i++)
     {
         v.push_back(i);
-        combi(i, v);
+        DeleteShop(i, v);
         v.pop_back();
     }
     return;
 }
-int main(){
+int main()
+{
     FAST_IO
-    cin >> n >> m;
-    for(int i = 0; i < n; i++)
+    // M개를 폐업 시키기
+    // maxNum = 0; 각 치킨의 거리를 계산 ret += (r-c) + (cr + cc) = ?
+    // maxNum = max(ret, maxNum)
+    // 결론은 M개의 치킨집을 폐업 시켰을때, 남은 치킨집으로 가장 최단거리를 찾는거
+
+    cin >> N >> M;
+    board.assign(N, vector<int>(N));
+    
+    for(int i = 0; i < N; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j  = 0; j < N; j++)
         {
-            cin >> a[i][j];
-            if(a[i][j] == 1) _home.push_back({i,j});
-            if(a[i][j] == 2) chicken.push_back({i,j});
+            cin >> board[i][j];
+            if(board[i][j] == 1)
+            {
+                hV.push_back({i,j});
+            }
+            if(board[i][j] == 2)
+            {
+                sV.push_back({i,j});
+            }
         }
     }
     vector<int> v;
-    combi(-1, v);
-    for(vector<int> cList : chickenList)
+    DeleteShop(-1, v);
+    for(vector<int> cL : chicken)
     {
         int ret = 0;
-        for(pair<int, int> home : _home)
+        for(pair<int, int> home : hV)
         {
-            int _min = 987654321;
-            for(int ch : cList)
+            int minNum = 1e9;
+            for(int c : cL)
             {
-                int _dist = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
-                _min = min(_min, _dist);
+                int dist = abs(home.first - sV[c].first) + abs(home.second - sV[c].second);
+                minNum = min(minNum, dist);
             }
-            ret += _min;
+            ret += minNum;
         }
-        result = min(result, ret);
+        res = min(res, ret);
     }
-    cout << result << '\n';
+
+    cout << res << endl;
+    
 }
