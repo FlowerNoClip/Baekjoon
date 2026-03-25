c26949727987233a570c6fd1ea5b47f45ad7886a
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-03-25 22:26:21 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,46 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
 using namespace std;
-vector<int> num;
-vector<char> oper_str;
-int n, ret = -987654321;
-string s;
-int oper(char a, int b, int c)
+int N;
+vector<int> number;
+vector<char> opers;
+string str;
+long long maxNum = -2e18;
+int cal(int a, int b, char op)
 {
-    if(a == '+') return b + c;
-    if(a == '-') return b - c;
-    if(a == '*') return b * c;
-}
-void go(int here, int _num)
-{
-    if(here == num.size() - 1)
-    { 
-        ret = max(ret, _num); 
-        return;
-    }  
-    go(here + 1, oper(oper_str[here], _num, num[here + 1]));
-    
-
-    if(here + 2 <= num.size() - 1)
-    {
-        int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]); 
-        go(here + 2, oper(oper_str[here], _num, temp));  
-    } 
-    return;
-} 
-
-int main()
-{   
-    FAST_IO;
-    cin >> n >> s;
-    for(int i = 0; i < n; i++)
-    {
-        if(i % 2 == 0) num.push_back(s[i] -'0');
-        else oper_str.push_back(s[i]); 
-    }
-    go(0, num[0]);
-    cout << ret << '\n';
+    if(op == '+') return a + b;
+    if(op == '*') return a * b;
+    if(op == '-') return a - b;
     return 0;
 }
+void solved(int idx, int cur)
+{
+    if(idx >= opers.size())
+    {
+        maxNum = max(maxNum, (long long)cur);
+        return;
+    }
 
-//현재 num 사이즈는 5 
+    int res1 = cal(cur, number[idx + 1], opers[idx]);
+    solved(idx + 1, res1);
+
+    if(idx+1 < opers.size())
+    {
+        int bracket = cal(number[idx + 1], number[idx + 2], opers[idx+1]);
+        int res2 = cal(cur, bracket, opers[idx]);
+        solved(idx + 2, res2);
+    }
+}
+int main()
+{
+    FAST_IO
+    cin >> N;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(i % 2 == 0)
+        {
+            number.push_back(str[i]-'0');
+        }
+        else
+        {
+            opers.push_back(str[i]);
+        }
+    }
+    if(N == 1)
+    {
+        cout << number[0];
+        return 0;
+    }
+    solved(0, number[0]);
+    cout << maxNum;
+    
+}
