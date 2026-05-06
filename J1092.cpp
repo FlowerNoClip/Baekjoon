fa86961052faea208e3602db6e581b6b2196de9e
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2026-05-06 20:22:28 +0900

    Add files via upload

---
+++ J1092.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+using namespace std;
+
+ll divide(ll a, ll b, ll c)
+{
+    if(b==0) return 1 % c;
+    if(b==1) return a % c;
+
+    ll temp = divide(a, b/2, c);
+    temp = (temp * temp) % c;
+    if(b % 2 == 1) temp = (temp * a) % c;
+    return temp % c;
+}
+
+int main()
+{
+    FAST_IO
+    ll A, B;
+    ll C = 20091024;
+    
+    cin >> A >> B;
+    if(A == 0 && B == 0)
+    {
+        cout << 1 << '\n';
+        return 0;
+    }
+    
+    cout << divide(A, B, C) ;
+}
